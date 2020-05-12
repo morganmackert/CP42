f06b47e37fc89ff6dfc0f9c26848f3c69725f88b
morgan.mackert <morgan.mackert@gmail.com>
2020-05-12 17:18:42 -0500

    Updates

--- FigureScripts/PreliminaryPlots.R
+++ FigureScripts/PreliminaryPlots.R
@@ -22,10 +22,12 @@ library(ggplot2)
 veg <- read.csv("Vegetation/CP42 Total Habitat Resources.csv", na.strings = c("", "NA"))
 no.bees <- read.csv("Bees/CP42 Bee Abundance by Site & Month.csv", na.strings = c("", "NA"))
 no.beespp <- read.csv("Bees/CP42 Number Bee Species by Site & Month.csv", na.strings = c("", "NA"))
+landuse <- read.csv("Sites/Land Use.csv", na.strings = c("", "NA"))
 
 #Join veg and bee data together
-beeshabita <- left_join(veg, no.bees, by = c("Month", "Site"))
-beeshabitat <- left_join(beeshabita, no.beespp, by = c("Month", "Site"))
+beeshabit <- left_join(veg, no.bees, by = c("Month", "Site"))
+beeshabita <- left_join(beeshabit, no.beespp, by = c("Month", "Site"))
+beeshabitat <- left_join(beeshabita, landuse, by = c("Site"))
 
 #Change Month to factor
 beeshabitat$Month <- as.factor(beeshabitat$Month)
@@ -169,3 +171,75 @@ bsfs <- ggplot(beeshabitat,
   labs(x = "Number of Floral Species",
        y = "Number of Bee Species")
 bsfs
+
+#Bee Abundance ~ % Agriculture ####
+#-------------------------------------------------------------------#
+#                      Bee Abundance ~ % Agriculture                #
+#-------------------------------------------------------------------#
+bapa <- ggplot(beeshabitat,
+               aes(x = Ag,
+                   y = no.bees)) +
+  geom_point(aes(shape = Month,
+                 color = Site),
+             size = 3) +
+  geom_smooth(method = "glm",
+              color = "black",
+              size = 0.5) +
+  theme_bw() +
+  labs(x = "Agriculture (%)",
+       y = "Number of Bees")
+bapa
+
+#Bee Species ~ % Agriculture ####
+#-------------------------------------------------------------------#
+#                       Bee Species ~ % Agriculture                 #
+#-------------------------------------------------------------------#
+bspa <- ggplot(beeshabitat,
+               aes(x = Ag,
+                   y = no.beespp)) +
+  geom_point(aes(shape = Month,
+                 color = Site),
+             size = 3) +
+  geom_smooth(method = "glm",
+              color = "black",
+              size = 0.5) +
+  theme_bw() +
+  labs(x = "Agriculture (%)",
+       y = "Number of Bee Species")
+bspa
+
+#Bee Abundance ~ % Natural ####
+#-------------------------------------------------------------------#
+#                        Bee Abundance ~ % Natural                  #
+#-------------------------------------------------------------------#
+bapn <- ggplot(beeshabitat,
+               aes(x = Natural,
+                   y = no.bees)) +
+  geom_point(aes(shape = Month,
+                 color = Site),
+             size = 3) +
+  geom_smooth(method = "glm",
+              color = "black",
+              size = 0.5) +
+  theme_bw() +
+  labs(x = "Natural Land (%)",
+       y = "Number of Bees")
+bapn
+
+#Bee Species ~ % Natural ####
+#-------------------------------------------------------------------#
+#                        Bee Species ~ % Natural                  #
+#-------------------------------------------------------------------#
+bspn <- ggplot(beeshabitat,
+               aes(x = Natural,
+                   y = no.beespp)) +
+  geom_point(aes(shape = Month,
+                 color = Site),
+             size = 3) +
+  geom_smooth(method = "glm",
+              color = "black",
+              size = 0.5) +
+  theme_bw() +
+  labs(x = "Natural Land (%)",
+       y = "Number of Bee Species")
+bspn
